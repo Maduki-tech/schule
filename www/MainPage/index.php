5aa99e4c57d5c88ea0bbf84c5d5fa4f077c609c3
David Schlueter <david.schlueter@divcon.de>
2021-12-16 14:44:42 +0100

    wip

---
+++ www/MainPage/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="style.css">
+    <title>Verwaldungssoftware</title>
+</head>
+
+
+<!-- https://www.youtube.com/embed/rCW8ELDwcIM?autohide=1&autoplay=1&fs=0&showinfo=0&modestBranding=1&start=0&controls=0&rel=0&disablekb=1&iv_load_policy=3&wmode=transparent&enablejsapi=1&origin=https%3A%2F%2Fwww.braveheartbattle.de&widgetid=1 -->
+
+<body>
+
+
+
+</body>
+
+</html>
